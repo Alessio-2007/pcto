52fb7fc0909c02c96d3ff528082164024e027195
Alessio-2007 <162116583+Alessio-2007@users.noreply.github.com>
2024-03-04 16:01:41 +0000

    fj

---
+++ esercizio01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int numero;
+    numero=10;
+printf("%d",numero);
+}
+ 
---
+++ esercizio02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main ()
+{
+    char lettera;
+    lettera='a';  
+    printf("%d",lettera);
+return(0);
+}   
+
